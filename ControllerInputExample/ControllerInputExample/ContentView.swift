9a42c496fd94c398bd861e86247d5c036c32b722
Jacob Martin <martinjaco@gmail.com>
2023-04-01 19:23:59 -0400

    controller hud WIP

--- ControllerInputExample/ControllerInputExample/ContentView.swift
+++ ControllerInputExample/ControllerInputExample/ContentView.swift
@@ -18,157 +18,192 @@ struct ContentView: View {
     var body: some View {
         home
             .padding()
-//            .frame(width: 500, height: 350)
+        //            .frame(width: 500, height: 350)
     }
 
-    var triggers: some View {
-        VStack {
-            HStack(alignment: .center) {
-                Spacer()
+    var leftTriggers: some View {
+        GeometryReader { proxy in
+            let dimension = proxy.size.width/5
+            VStack {
                 if let l2 = gamepad.elements[id: "l2"] {
                     Image(systemName: l2.state ? l2.pressed : l2.released)
+                        .resizable()
+                        .frame(width: dimension, height: dimension)
                         .offset(y: CGFloat(l2.value*16)-8).font(.title)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(colorChange(gamepad.connected), lineWidth: 2))
                 }
-                Spacer()
-                if let r2 = gamepad.elements[id: "r2"] {
-                    Image(systemName: r2.state ? r2.pressed : r2.released)
-                        .offset(y: CGFloat(r2.value*16)-8).font(.title)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-                Spacer()
-            }
-            HStack(alignment: .center) {
-                Spacer()
                 if let l1 = gamepad.elements[id: "l1"] {
                     Image(systemName: l1.state ? l1.pressed : l1.released)
+                        .resizable()
+                        .frame(width: dimension, height: dimension)
                         .offset(y: CGFloat(l1.value*16)-8).font(.title)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(colorChange(gamepad.connected), lineWidth: 2))
                 }
-                Spacer()
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
+        }
+    }
+
+    var rightTriggers: some View {
+        GeometryReader { proxy in
+            let dimension = proxy.size.width/5
+            VStack {
+                if let r2 = gamepad.elements[id: "r2"] {
+                    Image(systemName: r2.state ? r2.pressed : r2.released)
+                        .resizable()
+                        .frame(width: dimension, height: dimension)
+                        .offset(y: CGFloat(r2.value*16)-8)
+                }
+
                 if let r1 = gamepad.elements[id: "r1"] {
                     Image(systemName: r1.state ? r1.pressed : r1.released)
-                        .offset(y: CGFloat(r1.value*16)-8).font(.title)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(colorChange(gamepad.connected), lineWidth: 2))
+                        .resizable()
+                        .frame(width: dimension, height: dimension)
+                        .offset(y: CGFloat(r1.value*16)-8)
                 }
-                Spacer()
+
             }
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
         }
     }
 
     var dPad: some View {
-        VStack {
-            HStack(alignment: .center) {
-                if let up = gamepad.elements[id: "up"] {
-                    Image(systemName: up.state ? up.pressed : up.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
+        GeometryReader { proxy in
+            let dimension = proxy.size.width/5
+            Grid {
+                GridRow {
+                    Spacer()
+                    if let up = gamepad.elements[id: "up"] {
+                        Image(systemName: up.state ? up.pressed : up.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
+                }
+                GridRow {
+                    if let left = gamepad.elements[id: "left"] {
+                        Image(systemName: left.state ? left.pressed : left.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
+                    if let right = gamepad.elements[id: "right"] {
+                        Image(systemName: right.state ? right.pressed : right.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                }
+                GridRow {
+                    Spacer()
+                    if let down = gamepad.elements[id: "down"] {
+                        Image(systemName: down.state ? down.pressed : down.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
                 }
             }
-            HStack(alignment: .center) {
-                if let left = gamepad.elements[id: "left"] {
-                    Image(systemName: left.state ? left.pressed : left.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-                Spacer()
-                if let right = gamepad.elements[id: "right"] {
-                    Image(systemName: right.state ? right.pressed : right.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-            }
-            HStack(alignment: .center) {
-                if let down = gamepad.elements[id: "down"] {
-                    Image(systemName: down.state ? down.pressed : down.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-            }
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
         }
-        .font(.title)
-        .foregroundColor(colorChange(gamepad.connected))
-        .frame(width: 150, height: 150)
     }
 
     var shapes: some View {
-        VStack {
-            HStack(alignment: .center) {
-                if let triangle = gamepad.elements[id: "triangle"] {
-                    Image(systemName: triangle.state ? triangle.pressed : triangle.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
+        GeometryReader { proxy in
+            let dimension = proxy.size.width/5
+            Grid {
+                GridRow {
+                    Spacer()
+                    if let triangle = gamepad.elements[id: "triangle"] {
+                        Image(systemName: triangle.state ? triangle.pressed : triangle.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
+                }
+                GridRow {
+                    if let square = gamepad.elements[id: "square"] {
+                        Image(systemName: square.state ? square.pressed : square.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
+                    if let circle = gamepad.elements[id: "circle"] {
+                        Image(systemName: circle.state ? circle.pressed : circle.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                }
+                GridRow {
+                    Spacer()
+                    if let cross = gamepad.elements[id: "cross"] {
+                        Image(systemName: cross.state ? cross.pressed : cross.released)
+                            .resizable()
+                            .frame(width: dimension, height: dimension)
+                    }
+                    Spacer()
                 }
             }
-            HStack(alignment: .center) {
-                if let square = gamepad.elements[id: "square"] {
-                    Image(systemName: square.state ? square.pressed : square.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-                Spacer()
-                if let circle = gamepad.elements[id: "circle"] {
-                    Image(systemName: circle.state ? circle.pressed : circle.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-            }
-            HStack(alignment: .center) {
-                if let cross = gamepad.elements[id: "cross"] {
-                    Image(systemName: cross.state ? cross.pressed : cross.released)
-                        .padding(10)
-                        .overlay(Circle().stroke(colorChange(gamepad.connected), lineWidth: 2))
-                }
-            }
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
         }
-        .font(.title)
-        .foregroundColor(colorChange(gamepad.connected))
-        .frame(width: 150, height: 150)
     }
 
     var joysticks: some View {
-        HStack(alignment: .center) {
-            Spacer()
-            if let l3 = gamepad.elements[id: "l3"] {
-                Image(systemName: l3.state ? l3.pressed : l3.released)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                    .offset(
-                        x: CGFloat(l3.xvalue*20),
-                        y: -CGFloat(l3.yvalue*20))
-                    .padding()
+        GeometryReader { proxy in
+            let dimension = proxy.size.height/2
+            HStack(alignment: .center) {
+                Spacer()
+                if let l3 = gamepad.elements[id: "l3"] {
+                    Image(systemName: l3.state ? l3.pressed : l3.released)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: dimension, height: dimension)
+                        .offset(
+                            x: CGFloat(l3.xvalue*20),
+                            y: -CGFloat(l3.yvalue*20))
+                        .padding()
+                }
+                Spacer()
+                if let r3 = gamepad.elements[id: "r3"] {
+                    Image(systemName: r3.state ? r3.pressed : r3.released)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: dimension, height: dimension)
+                        .offset(
+                            x: CGFloat(r3.xvalue*20),
+                            y: -CGFloat(r3.yvalue*20))
+                        .padding()
+                }
+                Spacer()
             }
-            Spacer()
-            if let r3 = gamepad.elements[id: "r3"] {
-                Image(systemName: r3.state ? r3.pressed : r3.released)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                    .offset(
-                        x: CGFloat(r3.xvalue*20),
-                        y: -CGFloat(r3.yvalue*20))
-                    .padding()
-            }
-            Spacer()
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
         }
     }
 
     var home: some View {
-        VStack {
-            triggers
-            HStack {
-                dPad
+        ZStack {
+            VStack {
                 Spacer()
-                shapes
+                joysticks
+                    .frame(width: 300, height: 100)
             }
-            joysticks
+            HStack(spacing: 0) {
+                VStack {
+                    leftTriggers
+                        .frame(width: 150, height: 100)
+                    dPad
+                        .foregroundColor(colorChange(gamepad.connected))
+                        .frame(width: 150, height: 100)
+                }
+                Spacer()
+                VStack {
+                    rightTriggers
+                        .frame(width: 150, height: 100)
+                    shapes
+                        .foregroundColor(colorChange(gamepad.connected))
+                        .frame(width: 150, height: 100)
+                }
+            }
         }
-        .padding(.horizontal)
+        .frame(width: 400, height: 350)
+        .scaleEffect(0.5)
     }
 
 
